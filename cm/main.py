fff4e9dd96b07c1e7cebc26183152824464b9fe9
Васин Юрий <george.a.wise@gmail.com>
2021-02-28 12:42:13 +0300

    Update main.py

--- cm/main.py
+++ cm/main.py
@@ -11,23 +11,25 @@ true = True
 false = False
 cr_flag = False
 
+'''
 try:
-    from flask_cors import CORS
+	from flask_cors import CORS
 except ImportError:
-    print('CORS error')
+	print('CORS error')
 except Exception:
-    print('Unknown error')
+	print('Unknown error')
 else:
-    cr_flag = True
+	cr_flag = True
+'''
 
 # app = Flask(__name__, root_path='/api')
 # app.config["APPLICATION_ROOT"] = "/api"
 app = Flask(__name__)
 
 if cr_flag:
-    # CORS(app)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-    
+	# https://flask-cors.readthedocs.io/en/latest/
+	cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 api = Api(app)
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -35,26 +37,26 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 @app.route('/')
 def index():
-    return 'REST SERVER!'
+	return 'REST SERVER!'
 
 class GetData(Resource):
-    
-    class Spanish(Resource):
-        def get_data(self):
-            with open(os.path.join(__location__, "./data.yaml"), 'r', encoding="utf-8") as stream:
-                try:
-                    vocab = yaml.safe_load(stream)
-                    return random.choice(list(vocab.items()))
-                except yaml.YAMLError as exc:
-                    print(exc)
+	
+	class Spanish(Resource):
+		def get_data(self):
+			with open(os.path.join(__location__, "./data.yaml"), 'r', encoding="utf-8") as stream:
+				try:
+					vocab = yaml.safe_load(stream)
+					return random.choice(list(vocab.items()))
+				except yaml.YAMLError as exc:
+					print(exc)
 
-        #@app.route('api/GetData/Spanish', methods=['GET'])
-        def get(self):
-            return jsonify( self.get_data() )
-            # return self.get_data()
+		#@app.route('api/GetData/Spanish', methods=['GET'])
+		def get(self):
+			return jsonify( self.get_data() )
+			# return self.get_data()
 
-        #@app.route('api/GetData/Spanish', methods=['POST'])
-        def post(self):
-            return jsonify('Target method not supported!')
+		#@app.route('api/GetData/Spanish', methods=['POST'])
+		def post(self):
+			return jsonify('Target method not supported!')
 
 api.add_resource(GetData.Spanish, '/api/GetData/Spanish')
